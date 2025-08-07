d0ce911e06e77633731cdeaf80b9980adbdea6be
DINESHKUMAR <dineshkumarr.acc@gmail.com>
2025-08-07 13:59:30 +0530

    Update connectivity.py
    PYTHON AND MYSQL CONNECTIVITY
    -Just practice purpose
    -user data is personal ~ don't use  the same data

--- connectivity.py
+++ connectivity.py
@@ -1,11 +1,13 @@
 import mysql.connector
+
 # connect to MySQL
 conn = mysql.connector.connect(host="localhost",user="root",password="2004",database="connective")
 method=conn.cursor()
-# table creation
-# method.execute("""create table contacts(id int auto_increment primary key,
-# name varchar(200),
-# mobile_num varchar(15));""")
+table creation
+method.execute("""create table contacts(id int auto_increment primary key,
+name varchar(200),
+mobile_num varchar(15));""")
+
 # function to insert a record
 def insert_values(name,mobile_num):
     v1= "insert into contacts(name,mobile_num)values(%s,%s)"
@@ -21,6 +23,7 @@ def view_values ():
     for row in rows:
         print("ID:",row[0],"| name: ",row[1],"| mobile number ",row[2])
         print("-----------------")
+        
 # update the record
 def update_record(contactID,new_name,new_mobile_num):
     v1="update contacts set name=%s,mobile_num=%s where id=%s"
@@ -28,16 +31,17 @@ def update_record(contactID,new_name,new_mobile_num):
     method.execute(v1,v2)
     conn.commit()
     print(f"Update ID to {contactID} to name :{new_name},mobile{new_mobile_num}")
+    
 # delete the record
 def delete_values(contact_id):
     v1="delete from contacts where id=%s"
     v2=(contact_id)
     method.execute(v1,v2)
     conn.commit()
-# insert_values("Adhi","8774577646")
-update_record(1,"Dinesh","485124545")
-
-# delete_values([6])
-view_values()
+    
+insert_values("Adhi","8774577646") # change the values
+update_record(1,"Dinesh","485124545") # change the values
+delete_values([5]) #delete the data from table
+view_values() #To check values in table
 method.close()
 conn.close()
